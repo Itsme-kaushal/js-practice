76238396c2b40fafa1f1d140822af9822b2bac3a
Itsme-kaushal <139057691+Itsme-kaushal@users.noreply.github.com>
2025-02-13 12:16:02 +0000

    updated

--- 01_basics/01_variables.js
+++
@@ -1,3 +0,0 @@
-const accountId = 144553
-let accountEmail = "kaushal@gmail.com"
-var accountPassword = "12345678"
---
+++ 01_basics/01_variables.ts
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "kaushal@gmail.com"
+var accountPassword = "12345678"
+
+let apples : number = 5;
+let speet :string = 'fast';
+let nothing: undefined = undefined;
+let now: Date = new Date()
+let colors: string[]  = ['red','green']
+let myNumbers: number[] = [1,2,3]
+console.table([accountEmail,apples,speet,nothing,accountPassword,Date,colors,myNumbers]);
