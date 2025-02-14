ec9cb2ab2b209c24453ccecf71c318dffacefa1c
Itsme-kaushal <139057691+Itsme-kaushal@users.noreply.github.com>
2025-02-14 06:54:27 +0000

    updated

--- 01_basics/01_variables.ts
+++ 01_basics/01_variables.ts
@@ -2,6 +2,8 @@ const accountId = 144553
 let accountEmail = "kaushal@gmail.com"
 var accountPassword = "12345678"
 //type annotations when we declare a variable on one line then initialize it later
+//when functions return any type and we need to clarify the value
+// when we want a variable whose type should not  be inferred
 
 let apples : number = 5;
 let speet :string = 'fast';
@@ -14,3 +16,35 @@ let point :{ x : number; y :number} = {
     x:10,
     y:20
 }
+// 1) function that returns any type    
+const json = '{"x": 10, "y": 20}';
+//json.parse return any type but we can explicitly declare type of variable
+const coordinates :{x:number; y:number} = JSON.parse(json);
+console.log(coordinates);
+// console.log(typeof "x");
+//2) when we declare intially and intialise variable later
+let words = ['red','green','blue']
+let isgreenfound : boolean;
+
+for(let i = 0; i < words.length;i++){
+    if(words[i] == 'green'){
+        isgreenfound = true;
+        console.log(isgreenfound);
+    }
+}
+console.log(typeof isgreenfound);
+// variables whose type cant be inferred correclty even if intialization and declaration on the same line 
+let num = [ -10, -1 ,12];
+let numabovezero = false;
+console.log(typeof numabovezero);
+for (let index = 0; index < num.length; index++) {
+    if (num[index]  > 0) {
+        numabovezero = num[index];
+    }  
+}
+console.log(typeof numabovezero);
+console.log(numabovezero);
+
+
+
+
