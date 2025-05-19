14b877ec3533faa61ef0717f04bf61508dd26397
Itsme-kaushal <139057691+Itsme-kaushal@users.noreply.github.com>
2025-05-19 09:13:54 +0000

    increment operator

--- 01_basics/03_conversioOps.js
+++ 01_basics/03_conversioOps.js
@@ -7,4 +7,16 @@ console.log(isLoggedIn)
 // **************** ops*************
 
 console.log("1"+2);
- 
+
+console.log(+true);
+
+// prefix and postfix operator
+let x = 3;
+let a = 3
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+
+//++a first increment then assign and a++ first assign then implement
+
+const z = ++a;
+console.log(`a:${a}, z:${z}`);
