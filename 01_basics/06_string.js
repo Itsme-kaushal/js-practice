6b9076cc72b27a29dd85fca1428fa337db66a67b
Itsme-kaushal <139057691+Itsme-kaushal@users.noreply.github.com>
2025-06-18 07:53:09 +0000

    fix: added string interpolation

---
+++ 01_basics/06_string.js
@@ -0,0 +1,11 @@
+
+const name = 'kaushal'
+
+const repo = 50
+
+
+//outdated way to use string concatenation instead use string interpolation
+// console.log(name +" "+ repo)
+
+//we use backticks to use string interpolation
+console.log(`My name is ${name} and I have ${repo} repositories. `)
