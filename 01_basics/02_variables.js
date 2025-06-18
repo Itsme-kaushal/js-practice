bcc7ac2ed13b53a7ab4fe4258bd8b896eb0d0fd9
Itsme-kaushal <139057691+Itsme-kaushal@users.noreply.github.com>
2025-06-18 07:04:12 +0000

    feature:added variable.js

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,19 @@
+
+//cant change const
+const accountId = 144553
+
+let accountEmail = "Kaushal@google.com"
+/*
+prefer not to use var 
+because it does not'
+ have local scope
+ */
+var accountPass = "jaipur"
+
+accountCity = 'Delhi'
+
+// accountId = 2
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPass,accountCity])
