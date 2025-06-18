e4a5bbe2db52d8a9f83b83a4a468025d982a8b8e
Itsme-kaushal <139057691+Itsme-kaushal@users.noreply.github.com>
2025-06-18 08:06:44 +0000

    fix: added string methods

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -9,3 +9,25 @@ const repo = 50
 
 //we use backticks to use string interpolation
 console.log(`My name is ${name} and I have ${repo} repositories. `)
+
+const game = new String('Mario Kart');
+console.log(game);
+console.log(game.__proto__); // Access the prototype of the String object
+
+// String methods
+console.log(game.length); // Length of the string
+console.log(game.toUpperCase()); // Convert to uppercase
+console.log(game.toLowerCase()); // Convert to lowercase
+console.log(game.indexOf('Kart')); // Find the index of a substring
+console.log(game.charAt(0)); // Get character at index 0
+console.log(game.substring(0, 5)); // Get substring from index 0 to 5
+console.log(game.split(' ')); // Split the string into an array by spaces
+console.log(game.replace('Mario', 'Luigi')); // Replace a substring
+//original string remains unchanged
+console.log(game); // Output: Mario Kart
+
+// String immutability
+const originalString = 'Hello';
+const modifiedString = originalString.replace('H', 'J');
+console.log(originalString); // Output: Hello
+console.log(modifiedString); // Output: Jello
